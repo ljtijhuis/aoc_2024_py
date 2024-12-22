b3b7c8458a2672e220a1c4330d4e40b325ba7ca4
Lars Tijhuis <ljtijhuis@gmail.com>
2024-12-22 17:59:49 -0500

    Day 21 part 2

--- src/day21.py
+++ src/day21.py
@@ -154,6 +154,68 @@ class RobotSequenceExtended(RobotSequence):
         for i in range(25):
             self.robots.append(DirectionsRobot())
 
+def depth_first_search(sequence, robot_i, max_robot, mem):
+    if robot_i == max_robot:
+        return len(sequence)
+    
+    key = "".join(sequence) + str(robot_i)
+    if key in mem:
+        return mem[key]
+
+    robot = DirectionsRobot() if robot_i > 0 else NumpadRobot()
+    pos = robot.position
+
+    instructions_count = 0
+
+    # what moves do I need to do for robot i?
+    for s in sequence:
+        moves, pos = get_moves(s, pos, robot_i)
+
+        # now feed the moves one by one to the robots down the line, increasing count as we go
+        # for m in moves:
+        instructions_count += depth_first_search(moves, robot_i + 1, max_robot, mem)
+
+    mem[key] = instructions_count
+
+    # return the total count to perform all the moves
+    return instructions_count
+
+def get_moves(s, start, robot_i):
+    robot = DirectionsRobot() if robot_i > 0 else NumpadRobot()
+    target = robot.get_key_position(s)
+    d_r = target[0] - start[0]
+    d_c = target[1] - start[1]
+    if d_r == 0 and d_c == 0:
+        # Press A
+        return ['A'], target
+    elif d_r == 0:
+        # We only move along column axis
+        c = '<' if d_c < 0 else '>'
+        return [c for i in range(abs(d_c))] + ['A'], target
+    elif d_c == 0:
+        # we move only along row axis
+        c = '^' if d_r < 0 else 'v'
+        return [c for i in range(abs(d_r))] + ['A'], target
+    else:
+        # We move across both, which multiplies possible shortest paths by two
+        c = '^' if d_r < 0 else 'v'
+        row_moves = [c for i in range(abs(d_r))]
+        c = '<' if d_c < 0 else '>'
+        col_moves = [c for i in range(abs(d_c))]
+        
+        row_first = row_moves + col_moves + ['A']
+        col_first = col_moves + row_moves + ['A']
+        
+        if not robot.crosses_empty(start, row_moves + col_moves) and not robot.crosses_empty(start, col_moves + row_moves):
+            if d_c < 0:
+                return col_first, target
+            else:
+                return row_first, target
+        elif not robot.crosses_empty(start, row_moves + col_moves):
+            return row_first, target
+        else:
+            return col_first, target
+
 def read_file(f):
     with open(f) as opened_file:
         return opened_file.read()
@@ -162,15 +224,31 @@ def parse_input(text):
     codes = [list(line) for line in text.splitlines()]
     return codes
 
-text = read_file('day21_1.txt')
+text = read_file('day21.txt')
 codes = parse_input(text)
-# codes = ['37']
+# codes = ['083A']
 
-# robot_sequence = RobotSequence() # Use for part 1
-robot_sequence = RobotSequenceExtended() # Use for part 2
+robot_sequence = RobotSequence() # Use for part 1
+# robot_sequence = RobotSequenceExtended() # Use for part 2
 sum = 0
 for code in codes:
     sequence = robot_sequence.enter_sequence(code)
+    print(sequence)
     sum += len(sequence) * int("".join(code[0:len(code)-1]))
 
 print(sum)
+
+sum = 0
+for code in codes:
+    count = depth_first_search(code, 0, 3, {})
+    sum += count * int("".join(code[0:len(code)-1]))
+
+print(sum)
+
+sum = 0
+for code in codes:
+    count = depth_first_search(code, 0, 26, {})
+    sum += count * int("".join(code[0:len(code)-1]))
+
+print(sum)
+
